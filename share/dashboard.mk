8d269c76486164a83ec2becdc71a917ec08db0d7
Guillermo Miravalles <miravallesg@gmail.com>
2015-12-16 13:39:54 -0500

    Fix: change dist directory for app directory-1st working version

--- share/dashboard.mk
+++ share/dashboard.mk
@@ -1,15 +1,15 @@
 dashboardfontsdir = $(pkgdatadir)/webinterface/fonts
-dashboardfonts_DATA = webinterface/dashboard/dist/fonts/glyphicons-halflings-regular.eot webinterface/dashboard/dist/fonts/glyphicons-halflings-regular.svg webinterface/dashboard/dist/fonts/glyphicons-halflings-regular.ttf webinterface/dashboard/dist/fonts/glyphicons-halflings-regular.woff 
+dashboardfonts_DATA = webinterface/dashboard/app/fonts/glyphicons-halflings-regular.eot webinterface/dashboard/app/fonts/glyphicons-halflings-regular.svg webinterface/dashboard/app/fonts/glyphicons-halflings-regular.ttf webinterface/dashboard/app/fonts/glyphicons-halflings-regular.woff
 
 dashboardimagesdir = $(pkgdatadir)/webinterface/images
-dashboardimages_DATA = webinterface/dashboard/dist/images/5f583bc9.yeoman.png 
+dashboardimages_DATA = webinterface/dashboard/app/images/yeoman.png
 
 dashboardscriptsdir = $(pkgdatadir)/webinterface/scripts
-dashboardscripts_DATA = webinterface/dashboard/dist/scripts/f5850ac7.scripts.js webinterface/dashboard/dist/scripts/fecc5abc.vendor.js 
+dashboardscripts_DATA = webinterface/dashboard/app/scripts/app.js
 
 dashboardstylesdir = $(pkgdatadir)/webinterface/styles
-dashboardstyles_DATA = webinterface/dashboard/dist/styles/1d7769e0.vendor.css webinterface/dashboard/dist/styles/f36327c6.main.css 
+dashboardstyles_DATA = webinterface/dashboard/app/styles/main.css
 
 dashboardviewsdir = $(pkgdatadir)/webinterface/views
-dashboardviews_DATA = webinterface/dashboard/dist/views/main.html 
+dashboardviews_DATA = webinterface/dashboard/app/views/main.html
 
